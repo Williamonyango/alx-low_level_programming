355df5da27b87bed8a95baf6c9760937b55366dc
Onyangow <onyangowilliam857@gmail.com>
2023-06-15 15:15:37 +0300

    printing using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main-program that prints programming is like building a multilingual puzzle using puts
+ * return 0 when the program is succes
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
