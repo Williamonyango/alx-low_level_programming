cfec2a2d0b6720193a43d17523d9009e7dd8dc41
Onyangow <onyangowilliam857@gmail.com>
2023-06-15 22:07:30 +0300

    printing using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - prints "programming is like building a multilingual puzzle,
  * followed by a newline
- * return 0 success
+ * return: Always 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
