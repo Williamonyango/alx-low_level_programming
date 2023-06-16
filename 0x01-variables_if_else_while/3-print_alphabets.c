8ad898cae5ea97db188795d09d46a0e8d263b703
Onyangow <onyangowilliam857@gmail.com>
2023-06-16 22:13:00 +0300

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main block (prints alphabets in lower case
+ * followed by upper case
+ *Return: 0 when success
+ */
+int main(void)
+{
+	char c = 'A';
+	char y = 'a';
+
+	while (y <= 'z')
+	{
+		putchar(y);
+		y++;
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
