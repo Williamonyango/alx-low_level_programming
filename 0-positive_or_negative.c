6780887d47e1880d312e1a25ad6f6a76f32d0f82
Onyangow <onyangowilliam857@gmail.com>
2023-06-16 14:39:33 +0300

    Positive is better than negative nothing

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main block (sign a random number to a variable n each time
+ * it is executd
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int n;
+
+	strand(time(0));
+	n = rand() = RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n = 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is zero\n", n);
+	return (0);
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-0x00. C - Hello, World
+Program that assign a random number to the variables n each time it is executed
