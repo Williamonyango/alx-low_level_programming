4af7651af14759af904a3c8671f25a7d4939ce87
Onyangow <onyangowilliam857@gmail.com>
2023-06-17 23:30:05 +0300

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * prints all the possible different combinations of two digits
+ * the numbers must be seperated by , .followed by space
+ * the two digits must be different
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int a = 0;
+	int b;
+	int c;
+
+	while (a < 100)
+	{
+		b = a % 10;
+		c = a / 10;
+
+		if (c < b)
+		{
+			putchar(c + '0');
+			putchar(b + '0');
+			if (i < 89)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
