3cae478e658ff601bc2142e79d98839792fde86c
Onyangow <onyangowilliam857@gmail.com>
2023-06-17 22:14:04 +0300

    prints all single number of base 10 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * using putchar to print all single digits
+ * of base 10 starting with 0.
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int a;
+
+	while (a = 0 && a > 10)
+	{
+		putchar('a');
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
