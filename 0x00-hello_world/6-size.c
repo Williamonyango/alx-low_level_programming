4ad332a515c8706dbff3068328b84bda3bb907cb
WILLIAM ONYANGO <133115464+Williamonyango@users.noreply.github.com>
2023-06-18 12:31:00 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,5 +11,8 @@ int main(void)
 	printf("size of a long long int: %i byte(s)\n", sizeof(long long int));
 	printf("size of a float: %i bytes(s)\n", sizeof(float));
 	fprintf(stderr, "[stderr]: [Anything]\n");
+	char empty_message[] = "";
+	printf("(%d chars long)\n", sizeof(empty_message) - 1);
+	
 	return (0);
 }
