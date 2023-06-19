a160e7d458b48120615782b07fa68a17a288345b
Onyangow <onyangowilliam857@gmail.com>
2023-06-19 11:54:08 +0300

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	fprintf("size of a char: %d byte(s)\n", sizeof(char));
-	fprintf("Size of an int: %d byte(s)\n", sizeof(int));
-	fprintf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	fprintf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	fprintf("Size of a float: %d byte(s)\n", sizeof(float));
+	printf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
