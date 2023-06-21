d66969c8476925dd1c0a6059a39ed6ddbc9a7d31
Onyangow <onyangowilliam857@gmail.com>
2023-06-21 23:33:08 +0300

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - prints the fist 50 Fibonacci numbers starting with 1 and 2,
+ * followed by a new line.
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int i = 0;
+	long j = 1, k = 2;
+
+	while (i < 50)
+	{
+		if (i == 0)
+		{
+			printf("%ld", j);
+		}
+		else if (i == 1)
+		{
+			printf(", %ld", k);
+		}
+		else
+		{
+			k += j;
+			printf(", %ld", k);
+		}
+		++i;
+	}
+	printf("\n");
+	return (0);
+}
