f07172de40697522b991fdb75f1fe817079c0276
Williamonyango <ojwangwiliam@gmail.com>
2023-08-01 22:46:38 +0300

    More of singly linked lists

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * free_listint_safe - free a `listint_t` list and set the head to null
  * @h: double pointer to head of linked list
@@ -9,22 +8,22 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *current, *hold;
-	size_t count;
+  listint_t *current, *hold;
+  size_t count;
 
-	count = 0;
-	current = *h;
-	while (current != NULL)
-	{
-		count++;
-		hold = current;
-		current = current->next;
-		free(hold);
+  count = 0;
+  current = *h;
+  while (current != NULL)
+    {
+      count++;
+      hold = current;
+      current = current->next;
+      free(hold);
 
-		if (hold < current)
-			break;
-	}
-	*h = NULL;
+      if (hold < current)
+	break;
+    }
+  *h = NULL;
 
-	return (count);
+  return (count);
 }
