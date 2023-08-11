24f2972a5bd52368fa95484b7a8dcd05324c2ffc
Williamonyango <ojwangwiliam@gmail.com>
2023-08-11 15:33:56 +0300

    file.io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,53 +2,51 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
-/**
- *read_textfile - a function that reads a text file
- *and prints it to the POSIX standard output
- *@filename: the file name
- *@letters: the number of letters
- *Return: the actual number of letters or 0 if fails
- **/
 
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * @filename: The name of the file to be read.
+ * @letters: The maximum number of letters to read and print.
+ *
+ * Return: The actual number of letters read and printed, or 0 if fails.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  int fd;
-  char *buffer;
+	int fd;
+	char *buffer;
+	ssize_t _read, _write;
 
-  ssize_t _read, _write;
+	if (!filename)
+		return (0);
 
-  if(!filename)
-    {
-      return (0);
-    }
-  /*open the file*/
-  fd = open(filename, O_RDONLY);
-  if(fd == -1)
-    {
-      return (0);
-    }
-  buffer = malloc(sizeof(char) * letters);
-  if(buffer == NULL)
-    {
-      return (0);
-    }
-  /*read the file*/
-  _read = read(fd, buffer, letters);
-  if(_read == -1)
-    {
-      free(buffer);
-      close(fd);
-      return (0);
-    }
+	/* Open the file */
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-  /*write the file*/
-  _write = write(STDOUT_FILENO, buffer, _read);
-  if (_write == -1)
-    {
-      free(buffer);
-      close(fd);
-      return (0);
-    }
-  close(fd);
-  return (_read);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	/* Read the file */
+	_read = read(fd, buffer, letters);
+	if (_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	/* Write the file */
+	_write = write(STDOUT_FILENO, buffer, _read);
+	if (_write == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	close(fd);
+	free(buffer);
+	return (_read);
 }
