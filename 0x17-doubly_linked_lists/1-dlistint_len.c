1c4c7712879a8b3ea5a929aa0b2ead6887463d0d
Williamonyango <ojwangwiliam@gmail.com>
2023-10-14 01:01:08 +0300

    doubly linked lists

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 
-
 /**
  * dlistint_len - Counts the number of elements in a linked dlistint_t list.
  * @h: The head of the dlistint_t list.
@@ -9,23 +8,16 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    /*Variable to store the number of elements in the list.*/
-    size_t nodes = 0;
+	/*Variable to store the number of elements in the list.*/
+	size_t nodes = 0;
 
-
-    while (h)
-    {
-        /*Increment the element count.*/
-        nodes++;
-
-
-
-
-        /*Move to the next element.*/
-        h = h->next;
-    }
-
-
-    /*Return the total number of elements in the list.*/
-    return (nodes);
+	while (h)
+	{
+		/*Increment the element count.*/
+		nodes++;
+		/*Move to the next element.*/
+		h = h->next;
+	}
+	/*Return the total number of elements in the list.*/
+	return (nodes);
 }
--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,23 +1,23 @@
 #include "lists.h"
 
 
-/*
+/**
  * free_dlistint - Frees a linked dlistint_t list.
  * @head: The head of the dlistint_t list.
+ * Return: 0 if success
  */
 void free_dlistint(dlistint_t *head)
 {
-    /* Declare a temporary node pointer. */
-    dlistint_t *tmp;
+	/* Declare a temporary node pointer. */
+	dlistint_t *tmp;
 
-
-    while (head)
-    {
-        /* Store the next node in the temporary pointer. */
-        tmp = head->next;
-        /* Free the current node. */
-        free(head);
-        /* Move to the next node. */
-        head = tmp;
-    }
+	while (head)
+	{
+		/* Store the next node in the temporary pointer. */
+		tmp = head->next;
+		/* Free the current node. */
+		free(head);
+		/* Move to the next node. */
+		head = tmp;
+	}
 }
